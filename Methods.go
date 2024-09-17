cbc8af4e98cae7e5f3649ba5cdd11c293ca7694d
wangcwangc <858256500@qq.com>
2024-09-17 13:38:55 +0800

    update

--- Methods.go
+++ Methods.go
@@ -6,6 +6,10 @@ func method_a() {
 	fmt.Println("GoAPI_example: Method A")
 }
 
-func method_b() {
-	fmt.Println("GoAPI_example: Method B")
+func method_c() {
+	fmt.Println("GoAPI_example: Method C")
+}
+
+func method_d() {
+	fmt.Println("GoAPI_example: Method D")
 }
